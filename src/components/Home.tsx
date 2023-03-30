df7d45ee63743c07f0178703cc1784372ad71091
vladnen97 <darkdescent1991@gmail.com>
2023-03-30 04:00:45 +0300

    create home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Home = () => {
+    return (
+        <section className={'home'} id={'home'}>
+            <div className={'content'}>
+                <h3>Hello, It's Me</h3>
+                <h1>John Doe</h1>
+                <h3>And I'm a <span>Frontend Developer</span></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolorum esse fugit illum ipsum nisi provident. Eveniet.</p>
+            </div>
+            
+            <div className="photo">
+                <img src="https://placehold.jp/600x600.png" alt=""/>
+            </div>
+        </section>
+    );
+};
+
