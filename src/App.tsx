62ef2c6961a95d49b1fedfc74eb961114f92b303
vladnen97 <darkdescent1991@gmail.com>
2023-03-30 04:01:58 +0300

    import all comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import {Header} from './components/Header';
+import {Home} from './components/Home';
+import {Skills} from './components/Skills';
+import {Projects} from './components/Projects';
+import {Contact} from './components/Contact';
+import {Footer} from './components/Footer';
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Home/>
+            <Skills/>
+            <Projects/>
+            <Contact/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
