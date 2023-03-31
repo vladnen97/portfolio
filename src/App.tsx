b5721e2a5ac733041bf01d5084aa6e9c59463fd0
vladnen97 <darkdescent1991@gmail.com>
2023-03-31 19:59:32 +0300

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
-import {Header} from './components/Header';
-import {Home} from './components/Home';
-import {Skills} from './components/Skills';
-import {Projects} from './components/Projects';
-import {Contact} from './components/Contact';
-import {Footer} from './components/Footer';
+import {Header} from './components/Header/Header';
+import {Home} from './components/Home/Home';
+import {Skills} from './components/Skills/Skills';
+import {Projects} from './components/Projects/Projects';
+import {Contact} from './components/Contact/Contact';
+import {Footer} from './components/Footer/Footer';
+import {Hire} from './components/Hire/Hire';
 
 function App() {
     return (
@@ -14,6 +14,7 @@ function App() {
             <Home/>
             <Skills/>
             <Projects/>
+            <Hire/>
             <Contact/>
             <Footer/>
         </div>
