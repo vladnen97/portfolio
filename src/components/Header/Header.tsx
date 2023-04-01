3ed0713419b7bb3f82a498fc90e0b742f2dfc700
vladnen97 <darkdescent1991@gmail.com>
2023-04-01 19:54:30 +0300

    fix href, add state for burgerMenu control

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Header.module.css'
 
+
 export const Header = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+
     return (
         <header className={s.header}>
-            <a href="src/components/Header/Header#home" className={s.logo}>Portfolio</a>
+            <a href="#home" className={s.logo}>Portfolio</a>
 
-            {/*<i className='bx bx-menu' id={'burgerMenu'}></i>*/}
+            <i className={isOpen ? 'bx bx-x' : 'bx bx-menu'} id={s.menu} onClick={() => setIsOpen(!isOpen)}></i>
 
-            <nav className={s.navbar}>
-                <a href="src/components/Header/Header#home" className={s.active}>Home</a>
-                <a href="src/components/Header/Header#skills">Skills</a>
-                <a href="src/components/Header/Header#projects">Projects</a>
-                <a href="src/components/Header/Header#contact">Contact</a>
+            <nav className={s.navbar + (isOpen ? ' ' + s.active : '')}>
+                <a href="#home" onClick={() => setIsOpen(false)}>Home</a>
+                <a href="#skills" onClick={() => setIsOpen(false)}>Skills</a>
+                <a href="#projects" onClick={() => setIsOpen(false)}>Projects</a>
+                <a href="#contact" onClick={() => setIsOpen(false)}>Contact</a>
             </nav>
         </header>
     )
