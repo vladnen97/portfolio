97b99e863992374d597b6e3752a27a609e4e74c3
vladnen97 <darkdescent1991@gmail.com>
2023-06-25 22:38:16 +0300

    use particle comp

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import {Projects} from './components/Projects/Projects';
 import {Contact} from './components/Contact/Contact';
 import {Footer} from './components/Footer/Footer';
 import {Hire} from './components/Hire/Hire';
+import {Particle} from './components/Particle';
 
 function App() {
     return (
         <>
+            <Particle/>
             <Header/>
             <main>
                 <Home />
