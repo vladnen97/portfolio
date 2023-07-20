114d8c9e646919a26c0cac489c4cf21f58aa482b
vladnen97 <darkdescent1991@gmail.com>
2023-07-20 21:15:33 +0300

    feat: use images instead placeholder, add description and links

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,40 +2,44 @@ import React from 'react';
 import s from './Projects.module.css'
 import {Projectbox} from './Projectbox/Projectbox';
 import {Fade} from 'react-awesome-reveal';
+import  socialNetwork  from '../../assets/social-network.jpg'
+import  flashCards  from '../../assets/flashcards.png'
+import  toDoList  from '../../assets/to-do-list.jpg'
+import  homework  from '../../assets/homework.jpg'
 
 export const Projects = () => {
     const projects = [
         {
             id: 1,
-            img: 'https://placehold.jp/400x300.png',
-            linkGitHub: 'https://www.google.com',
-            linkDeploy: 'https://www.google.com',
+            img: socialNetwork,
+            linkGitHub: 'https://github.com/vladnen97/social-network',
+            linkDeploy: 'https://vladnen97.github.io/social-network',
             title: 'Social Network',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+            desc: 'An online application that is used for communication, dating, creating social relationships between people who have similar interests. React, Redux, TS, Axios, Ant Design'
         },
         {
             id: 2,
-            img: 'https://placehold.jp/400x300.png',
-            linkGitHub: 'https://www.google.com',
-            linkDeploy: 'https://www.google.com',
-            title: 'ToDo',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+            img: toDoList,
+            linkGitHub: 'https://github.com/vladnen97/todo-list',
+            linkDeploy: 'https://vladnen97.github.io/todo-list',
+            title: 'Todolist',
+            desc: 'Todolist is a to-do list and task manager. It will help you gain concentration, organization and peace. React, RTK, TS, MUI, Axios, StoryBook'
         },
         {
             id: 3,
-            img: 'https://placehold.jp/400x300.png',
-            linkGitHub: 'https://www.google.com',
-            linkDeploy: 'https://www.google.com',
+            img: homework,
+            linkGitHub: 'https://github.com/vladnen97/homework',
+            linkDeploy: 'http://vladnen97.github.io/homework',
             title: 'Homework',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+            desc: 'My homework assignments that I did during my studies.'
         },
         {
             id: 4,
-            img: 'https://placehold.jp/400x300.png',
+            img: flashCards,
             linkGitHub: 'https://www.google.com',
             linkDeploy: 'https://www.google.com',
             title: 'Cards',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+            desc: 'Not implemented yet'
         },
     ]
 
